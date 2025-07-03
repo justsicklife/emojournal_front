8e3a6a7fe0d73c71ec1a2656a879bd947b9f1f4e
justsicklife <justsicklife@gmail.com>
2025-07-03 18:19:05 +0900

    인증한 데이터 보여줌

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,37 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+
+  const [member,setMember] = useState({});
+
+  useEffect(() => {
+
+    const accessToken = localStorage.getItem("accessToken");
+
+    fetch("http://localhost:8080/member",{
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + accessToken
+      },
+      credentials: "include"
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      setMember(data);
+    })
+  },[]);
+
   return (
     <div>
       <h1>홈페이지</h1>
       <p>환영합니다! 구글 로그인에 성공하셨습니다.</p>
+      <div>
+          {member.email}
+      </div>
+      <div>
+        {member.nickname}
+      </div>
     </div>
   );
 };
