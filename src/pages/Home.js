d3b6ffd95683d4f23bb344fde4db6bb35e0a870a
justsicklife <justsicklife@gmail.com>
2025-07-09 16:13:31 +0900

    테스트 커밋

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,17 +7,31 @@ const Home = () => {
   const [member,setMember] = useState({});
 
   const getMemberInfo = async () => {
-    console.log("aaa");
     try {
-      const res = await api.get("/member")
-      console.log("회원 정보: ", res.data);
-      setMember(res.data);
+      if(localStorage.getItem("accessToken")) {
+        const res = await api.get("/member")
+        console.log("회원 정보: ", res.data);
+        setMember(res.data);
+      }
     } catch(err) {
       console.error("요청  실패 : " , err);
     }
   }
 
+  const logoutButton = () => {
+    try {
 
+      axios.post("http://localhost:8080/auth/logout",null , {
+        withCredentials: true,
+      });
+      
+      localStorage.removeItem("accessToken");
+      
+      console.log("로그아웃 완료")
+    } catch(err) {
+      console.error("로그아웃 실패")
+    }
+  } 
 
   useEffect(() => {
     getMemberInfo()
@@ -25,6 +39,9 @@ const Home = () => {
 
   return (
     <div>
+      <div>
+        <button onClick={logoutButton}>로그아웃</button>
+      </div>
       <h1>홈페이지</h1>
       <p>환영합니다! 구글 로그인에 성공하셨습니다.</p>
       <div>
